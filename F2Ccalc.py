8755f3731fc69d64a61e5e7e24be602b1c1ee8bb
Andy <104680626+volt-thrower@users.noreply.github.com>
2022-05-02 23:29:50 +0800

    Update F2Ccalc.py

--- F2Ccalc.py
+++ F2Ccalc.py
@@ -27,22 +27,9 @@ while tempF != 'q' and tempF != 'Q':
         tempC = FtoC(tempF) #calls on function FtoC
         print('==============================================')
         print(tempF, 'Degrees Fahrenheit') #prints temp in F
-        print(round(tempC, 2), "Degrees Celcius") #rounds and prints temp in C
+        print(round(tempC, 2), "Degrees Celsius") #rounds and prints temp in C
         print('==============================================')
         tempF = input("Enter Temp in Degrees F. Enter Q to quit:")
     except ValueError:
         #if input can't be converted to float
         tempF = input("Please enter a valid temperature:")
-        
-
-
-'''
-#Raw F to C with no loop
-def FtoC(tempF): #defining function FtoC with tempF var
-    print(tempF, 'Degrees Fahrenheit') #prints temp in F
-    return (tempF - 32) * (5/9) #function calcs  & returns temp in C
-    
-tempF = float(input("Enter Temp in Degrees F:"))#asks for input, type to float
-tempC = FtoC(tempF) #calls on function FtoC
-print(round(tempC, 2), "Degrees Celcius") #rounds and prints temp in C
-'''
